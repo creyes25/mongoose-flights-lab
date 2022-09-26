70453028c358ab653e37e1ff55e7697ce847656f
creyes25 <carolina.reyes2022@gmail.com>
2022-09-25 20:35:02 -0500

    Setup database connection and add controller files

---
+++ controllers/flights.js
@@ -0,0 +1,9 @@
+import { Flight } from "../models/flight.js"
+
+function index(req, res) {
+  res.send('respond with a resource')
+}
+
+export {
+  index
+}
---
+++ routes/flights.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import * as flightsCtrl from '../controllers/flights.js'
+
+const router = Router()
+
+/* GET users listing. */
+router.get('/', flightsCtrl.index)
+
+export {
+  router
+}
