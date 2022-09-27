f949009fe20181aab7dfb4bd4b41feec988e9b0e
creyes25 <carolina.reyes2022@gmail.com>
2022-09-27 08:03:19 -0500

    Add controller and router to create a new flight

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,9 +1,34 @@
 import { Flight } from "../models/flight.js"
 
+
+function newFlight(req, res) {
+  res.render('flights/new', {
+    title: 'Add Flight'
+  })
+}
+
+function createFlight(req, res) {
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights/new')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('flights/new')
+  })
+}
+
 function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    flights
+  })
   res.send('respond with a resource')
 }
 
+
 export {
-  index
+  index,
+  newFlight as new,
+  createFlight as create
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,8 +3,14 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
-/* GET users listing. */
+// GET - /flights
 router.get('/', flightsCtrl.index)
+
+// GET - /flights/new
+router.get('/new', flightsCtrl.new)
+
+// POST - /flights
+router.post('/', flightsCtrl.create)
 
 export {
   router
